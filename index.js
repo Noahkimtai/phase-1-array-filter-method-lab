6663207c12330593f1df698a4ee2a2199c909cf2
Noahkimtai <noahkimtai24@gmail.com>
2022-08-23 11:39:48 +0300

    Completed assignement

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(drivers, string){
+    let matching= drivers.filter((value)=>{
+
+      return value.toLowerCase()===string.toLowerCase();
+    });
+    return matching;
+}
+
+function fuzzyMatch(drivers, string){
+ let filtered = drivers.filter((value)=>{
+    return value.slice(0,2)==string.slice(0,2);
+    });
+ return filtered;
+}
+
+function matchName(drivers,string){
+let matching =drivers.filter((item)=>{
+    return item.name ===string
+})
+return matching;
+ }
